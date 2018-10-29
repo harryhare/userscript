4a1a54453d893bc4838bc8488d21d385a57cc989
Philip Jay Fry <harryhare@163.com>
2018-10-29 12:58:06 +0800

    revert abstract node height  and change indent

--- index2.js
+++ index2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bonnae News With Text
 // @namespace    https://github.com/harryhare/Bonnae-News
-// @version      0.6.2
+// @version      0.6.3
 // @description  for Bonnae broadcast on douban.com
 // @author       harryhare
 // @license      GPL 3.0
@@ -20,6 +20,7 @@
 	http://upaste.me/xxxxx
 	https://slexy.org/view/xxxxx
 	https://paste2.org/xxxxx
+	https://paste.ee/p/xxxxx
 */
 var url_prefix=new Map();
 url_prefix['upaste.me']='http://upaste.me/';
@@ -37,68 +38,68 @@ var url_node=new Map();
 var url_node_multi=new Map();
 
 function getContent1(doc){
-    return doc.getElementsByTagName('textarea')[0].innerHTML;
+	return doc.getElementsByTagName('textarea')[0].innerHTML;
 }
 function getContent2(doc){
-    return doc.getElementsByClassName('text')[0].innerHTML;
+	return doc.getElementsByClassName('text')[0].innerHTML;
 }
 function getContent3(doc){
-    return doc.getElementsByClassName('highlight code')[0].innerHTML;
+	return doc.getElementsByClassName('highlight code')[0].innerHTML;
 }
 function getContent4(doc){
-    return doc.getElementsByClassName('editor')[0].innerHTML;
+	return doc.getElementsByClassName('editor')[0].innerHTML;
 }
 function getContent(response){
-    var parser=new window.DOMParser();
-    var x=response.response;
-    var xmlDoc=parser.parseFromString(x,"text/html");
-    var url=response.finalUrl;
-    var text='';
-    var reg=/https?:\/\/([a-zA-Z0-9]+\.[a-z]+)(\/p\/)?/g;
-    var result=reg.exec(url);
-    if(result && result.length>1 && result[1] && url_getContent[result[1]]){
-        return url_getContent[result[1]](xmlDoc);
-    }
-    return "";
+	var parser=new window.DOMParser();
+	var x=response.response;
+	var xmlDoc=parser.parseFromString(x,"text/html");
+	var url=response.finalUrl;
+	var text='';
+	var reg=/https?:\/\/([a-zA-Z0-9]+\.[a-z]+)(\/p\/)?/g;
+	var result=reg.exec(url);
+	if(result && result.length>1 && result[1] && url_getContent[result[1]]){
+		return url_getContent[result[1]](xmlDoc);
+	}
+	return "";
 }
 
 function attachContent(response){
-    if(response.status!=200){
-        return;
-    }
-    if(!url_node[response.finalUrl]){
-        return;
-    }
-    for(var i=0;i<url_node[response.finalUrl].length;i++){
-        var node=url_node[response.finalUrl][i]
-        if(!node){
-            return;
-        }
-        var text=getContent(response);
-        if(!text){
-            return;
-        }
-        node.innerHTML=text;
-        node.style='height:20px; overflow: hidden;';
-    }
+	if(response.status!=200){
+		return;
+	}
+	if(!url_node[response.finalUrl]){
+		return;
+	}
+	for(var i=0;i<url_node[response.finalUrl].length;i++){
+		var node=url_node[response.finalUrl][i]
+		if(!node){
+		    return;
+		}
+		var text=getContent(response);
+		if(!text){
+		    return;
+		}
+		node.innerHTML=text;
+		node.style='height:45px; overflow: hidden;';
+	}
 }
 
 function onClick(e){
-    //console.log(e.target.attachId);
-    var n3=url_node_multi[e.target.attachId];
-    if(e.target.innerHTML=='收起'){
-        e.target.innerHTML='展开';
-        n3.style='height:52px; overflow: hidden;';
-    }else{
-        e.target.innerHTML='收起';
-        n3.style='';
-    }
-    e.stopPropagation();
+	//console.log(e.target.attachId);
+	var n3=url_node_multi[e.target.attachId];
+	if(e.target.innerHTML=='收起'){
+		e.target.innerHTML='展开';
+		n3.style='height:52px; overflow: hidden;';
+	}else{
+		e.target.innerHTML='收起';
+		n3.style='';
+	}
+	e.stopPropagation();
 }
 
 (function() {
-    'use strict';
-    var targets=document.querySelectorAll('.new-status .status-item[data-uid="1540691"] .mod .bd .status-saying blockquote p');
+	'use strict';
+	var targets=document.querySelectorAll('.new-status .status-item[data-uid="1540691"] .mod .bd .status-saying blockquote p');
 
 	for(let i=0;i<targets.length;i++){
 		var t=targets[i];
@@ -123,34 +124,34 @@ function onClick(e){
 			}
 		}
 		if(find){
-            t=t.parentElement;
+                        t=t.parentElement;
 			var n0=document.createElement('div');
 			var n1=document.createElement('blockquote');
 			var a=document.createElement('a');
-            var b=document.createElement('button');
+                        var b=document.createElement('button');
 			t.parentElement.appendChild(n0);
 			n0.appendChild(n1);
 			n1.appendChild(a);
-            n1.appendChild(b);
+                        n1.appendChild(b);
 			a.textContent=href;
 			a.setAttribute('href',href);
-            b.innerHTML='展开';
-            b.onclick=onClick;
-            var text=document.createElement('blockquote');
-            n0.appendChild(text);
-            if(url_node[href]){
-                url_node[href].push(text);
-            }else{
-                url_node[href]=[text];
-            }
-            b.attachId=href+url_node[href].length;
-            url_node_multi[b.attachId]=text;
-            text.id=href;
-            GM_xmlhttpRequest({
-                method: "GET",
-                url: href,
-                onload: attachContent,
-            });
-        }
+			b.innerHTML='展开';
+			b.onclick=onClick;
+			var text=document.createElement('blockquote');
+			n0.appendChild(text);
+			if(url_node[href]){
+			url_node[href].push(text);
+			}else{
+			url_node[href]=[text];
+			}
+			b.attachId=href+url_node[href].length;
+			url_node_multi[b.attachId]=text;
+			text.id=href;
+			GM_xmlhttpRequest({
+			method: "GET",
+			url: href,
+			onload: attachContent,
+			});
+		}
 	}
 })();
