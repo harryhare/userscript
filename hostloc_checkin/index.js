28cb1de99400ccfe18f7d79760e68a4db76c5fdf
harryhare <harryhare@163.com>
2018-11-02 03:27:19 +0800

    add hostloc checkin script

---
+++ hostloc_checkin/index.js
@@ -0,0 +1,61 @@
+// ==UserScript==
+// @name         hostloc checkin
+// @namespace    http://tampermonkey.net/
+// @version      0.3.1
+// @description  add button to get hostloc credit through visit others' space
+// @author       harryhare
+// @match        https://www.hostloc.com/**
+// @match        http://www.hostloc.com/**
+// @grant        none
+// ==/UserScript==
+
+
+var i=0;
+var button;
+function visit(){
+    if(i>=15){
+        return;
+    }
+    var xmlhttp=new XMLHttpRequest();
+    var url="/space-uid-"+(i+28939)+".html";
+    console.log('visit',url);
+    i++;
+    xmlhttp.open("GET",url,true); //第三个参数是同步异步,主线程只能异步
+    xmlhttp.onreadystatechange=visit;
+    xmlhttp.send();
+}
+
+
+//time delay
+var interval=500;
+function visit2() {
+    if(i>=15){
+        interval_id=window.clearInterval(interval_id);
+        return;
+    }
+    var xmlhttp=new XMLHttpRequest();
+    var url="/space-uid-"+i+".html";
+    console.log('visit',url);
+    i++;
+    button.innerHTML=String((i*100./15).toFixed(0))+"%";
+    xmlhttp.open("GET",url,true); //第三个参数是同步异步,主线程只能异步
+    xmlhttp.send();
+}
+var interval_id;
+
+(function() {
+    'use strict';
+
+    var target=document.getElementById("extcreditmenu");
+    if(!target){
+        return;
+    }
+    button=document.createElement('button');
+    button.innerHTML='签到';
+    button.onclick=(e)=>{
+        //visit();
+        interval_id= window.setInterval(visit2,interval);
+    };
+    target.parentElement.prepend(button);
+
+})();
