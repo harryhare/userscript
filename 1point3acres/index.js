cb1afa07553cb34d983500498cb8e57db40eed27
harryhare <harryhare@163.com>
2021-02-07 15:18:07 +0800

    add new user script for 1point3acres

--- 1point3acres/index.js
+++ 1point3acres/index.js
@@ -2,7 +2,7 @@
 // @name         一亩三分地 Anti Anti Adblock
 // @namespace    http://tampermonkey.net/
 // @version      0.0.1
-// @description  豆瓣广播详情页点赞坏掉，增加备用点赞按钮
+// @description  在开启Adblock 插件后，浏览网站会出现烦人的弹窗，这个插件会抑制弹窗出现
 // @author       harryhare
 // @license      GPL 3.0
 // @downloadURL  https://github.com/harryhare/userscript/raw/master/1point3acres/index.js
@@ -12,18 +12,36 @@
 // ==/UserScript==
 
 
-var callback = function (records){
-  records.map(function(record){
-    console.log('Mutation type: ' + record.type);
-    console.log('Mutation target: ' + record.target);
-  });
+var callback = function (records) {
+	records.map(function (record) {
+		if (record.addedNodes.length != 0) {
+			// console.log('Mutation type: ' + record.type);
+			// console.log('Mutation target: ');
+			// console.log(record.target);
+			// console.log(record.addedNodes);
+			for(var i=0;i<record.addedNodes.length;i++){
+				var node=record.addedNodes[i];
+				if(node.className=="fc-ab-root"){
+					// 直接删掉页面会卡住
+					// node.parentElement.removeChild(node);
+					// 模仿点击关闭
+					document.querySelector("button.fc-close").click();
+				}
+			}
+		}
+	});
 };
 
+(function () {
+	'use strict';
 
+	var mo = new MutationObserver(callback);
 
+	var option = {
+		'childList': true,
+		'subtree': false,
+	};
 
-(function() {
-    'use strict';
-	var observer = new MutationObserver(callback);
+	mo.observe(document.body, option);
 
 })();
